06aff75db534839b0d035e4c3b30e4b6d939541c
ALNAHMI711 <alamanyaalamanya06@gmail.com>
2026-09-08 21:12:10 +0300

    chore: remove accidental kline domain file from main

--- backend/app/market_data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Historical market-data domain."""
-
-from .models import Kline
-from .schemas import KlineInterval, KlineRecord
-from .validation import validate_kline
-
-__all__ = ["Kline", "KlineInterval", "KlineRecord", "validate_kline"]
