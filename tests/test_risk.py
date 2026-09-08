17a5254370194be58eb8e36c22367df07fc57467
ALNAHMI711 <alamanyaalamanya06@gmail.com>
2026-09-08 04:20:00 +0300

    style: organize risk test imports

--- tests/test_risk.py
+++ tests/test_risk.py
@@ -1,4 +1,5 @@
-from risk.engine import RiskLimits,evaluate
+from risk.engine import RiskLimits, evaluate
+
 L=RiskLimits(100,.2,2,1000,500,3,.002,.003)
 def test_kill_switch_always_blocks():
  assert not evaluate(L,daily_loss=0,open_positions=0,exposure=0,position_size=1,leverage=1,spread=0,slippage=0,kill_switch=True).approved
