7b969c395e0d145f96692bc35eef61ed2ab2770e
ALNAHMI711 <alamanyaalamanya06@gmail.com>
2026-09-08 04:01:40 +0300

    style: normalize market adapter declarations

--- backend/app/adapters/markets.py
+++ backend/app/adapters/markets.py
@@ -2,15 +2,35 @@ from .base import ExchangeAdapter, Market
 from .binance import BinanceAdapter
 from .market_depth import BinanceMarketDepthMixin
 
+
 class SpotAdapter(BinanceMarketDepthMixin, BinanceAdapter):
-    def __init__(self, *args, **kwargs): super().__init__(Market.SPOT, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(Market.SPOT, *args, **kwargs)
+
+
 class CrossMarginAdapter(BinanceMarketDepthMixin, BinanceAdapter):
-    def __init__(self, *args, **kwargs): super().__init__(Market.CROSS_MARGIN, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(Market.CROSS_MARGIN, *args, **kwargs)
+
+
 class IsolatedMarginAdapter(BinanceMarketDepthMixin, BinanceAdapter):
-    def __init__(self, *args, **kwargs): super().__init__(Market.ISOLATED_MARGIN, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(Market.ISOLATED_MARGIN, *args, **kwargs)
+
+
 class USDSMAdapter(BinanceMarketDepthMixin, BinanceAdapter):
-    def __init__(self, *args, **kwargs): super().__init__(Market.USDS_M, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(Market.USDS_M, *args, **kwargs)
+
+
 class COINMAdapter(BinanceMarketDepthMixin, BinanceAdapter):
-    def __init__(self, *args, **kwargs): super().__init__(Market.COIN_M, *args, **kwargs)
-class AlphaAdapter(ExchangeAdapter): market=Market.ALPHA
-class StockAdapter(ExchangeAdapter): market=Market.STOCKS
+    def __init__(self, *args, **kwargs):
+        super().__init__(Market.COIN_M, *args, **kwargs)
+
+
+class AlphaAdapter(ExchangeAdapter):
+    market = Market.ALPHA
+
+
+class StockAdapter(ExchangeAdapter):
+    market = Market.STOCKS
